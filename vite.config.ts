18069c966cacf0ba7810ec83623b57f1824d2760
Chenhao Liu <lizardll@foxmail.com>
2025-11-18 03:23:52 +0000

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/yunnan-tourists-website-demo/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
